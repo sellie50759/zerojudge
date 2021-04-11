3a6a1dd989790782be23f8f1fb47f4396d3b7905
sellie50759 <81401079+sellie50759@users.noreply.github.com>
2021-04-11 20:59:33 +0800

    Create f728.cpp

---
+++ f728.cpp
@@ -0,0 +1,27 @@
+include <iostream>
+#include <string>
+#include <set>
+#include<vector>
+#include <map>
+#include<cstring>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+#define MAX 10000000
+int main()
+{
+    int k;
+    double p;
+    while(cin>>k){
+        if(k==0){
+            cout<<2<<"\n";
+            continue;
+        }
+        p=1-double(k)/100;
+        int ans=1;
+        double now=1;
+        for(int i=364;now>p;i--,ans++)
+            now*=i/365.0;
+        cout<<ans<<"\n";
+    }
+}
