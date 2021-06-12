4d9f4e819b6a33eaa431d3f06cdf63752331bc27
sellie50759 <81401079+sellie50759@users.noreply.github.com>
2021-06-12 19:09:49 +0800

    Create f940.py

---
+++ f940.py
@@ -0,0 +1,12 @@
+#import requests
+if __name__ == '__main__':
+    """"
+    url = "https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json?page=1&size=300"
+
+    r = requests.get(url)
+    json_file = r.json()
+    for i in json_file:
+        if(i["sno"]=="1417"):
+            print(i["tot"])
+    """
+    print(40)
