c6ab6fd6f8db4f02bd1ef0c9393987bbade77b6a
sellie50759 <81401079+sellie50759@users.noreply.github.com>
2021-06-19 02:40:34 +0800

    Create c001.cpp

---
+++ c001.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<iomanip>
+using namespace std;
+typedef long long ll;
+#define MAX 1005
+int LCS[MAX][MAX];
+int main() {
+    string s,t;
+    while(cin>>s>>t){
+        for(int i=0;i<s.size();i++)
+            LCS[i][0]=0;
+        for(int i=0;i<t.size();i++)
+            LCS[0][i]=0;
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                if(s[i-1]==t[j-1])//遞迴式
+                    LCS[i][j]=LCS[i-1][j-1]+1;
+                else LCS[i][j]=max(LCS[i-1][j],LCS[i][j-1]);
+            }
+        }
+        cout<<LCS[s.size()][t.size()]<<"\n";
+    }
+}
