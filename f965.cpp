114d3fb20146a7a7689976a719d9f5874a2ce7b7
sellie50759 <81401079+sellie50759@users.noreply.github.com>
2021-06-20 01:35:34 +0800

    Update f965.cpp

--- f965.cpp
+++ f965.cpp
@@ -1,3 +1,45 @@
+/*因為我正在讀OS，所以我就出了這題。
+
+現在請各位寫一支程式模擬以下這些指令：
+
+1. touch [name]：如果當前資料夾下沒有叫做 [name] 的檔案或資料夾，則在當前資料夾下新增一個叫做 [name] 的檔案。
+
+2. mkdir [name]：如果當前資料夾下沒有叫做 [name] 的檔案或資料夾，則在當前資料夾下新增一個叫做 [name] 的資料夾。否則，印出一行 mkdir: [name]: File exists。
+
+3. cd [name]：
+
+i. 如果 [name] 是 .. 且當前資料夾不是 /，則前往上層資料夾。
+
+ii. 如果 [name] 不是 .. 且當前資料夾下沒有叫做 [name] 的檔案或資料夾，則印出一行 cd: [name]: No such file or directory。
+
+iii. 如果 [name] 不是 .. 且當前資料夾下有叫做 [name] 的檔案，則印出一行 cd: [name]: Not a directory。
+
+iv. 如果 [name] 不是 .. 且當前資料夾下有叫做 [name] 的資料夾，則前往該資料夾。
+
+4. ls：依照字典序由小到大印出所有當前資料夾下的檔案和資料夾，一行一個。
+
+程式開始前當前資料夾為 / 且 / 下沒有任何檔案或資料夾。
+
+範例輸入 #1
+11
+touch b
+mkdir b
+touch b
+mkdir a
+cd a
+cd a
+touch a
+cd a
+cd ..
+cd ..
+ls
+範例輸出 #1
+mkdir: b: File exists
+cd: a: No such file or directory
+cd: a: Not a directory
+a
+b
+*/
 #include<iostream>
 #include<cstring>
 #include<vector>
